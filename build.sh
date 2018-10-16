08c1e40cef19f4434fb6298a01f47eb4c61d785d
depohmel <ilya@kasten.io>
2018-10-16 14:14:28 -0700

    Adding server generation
    modifing swagger to support /store
    adding ImageMeta and ImageData models
    adding serevr generation in build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,25 @@ GOPATH=$(pwd)/go swagger generate client --spec=swagger.yaml -t go/src/demoapp \
             --skip-validation \
             --client-package=restclient \
             --template-dir=build/templates
+
+GOPATH=$(pwd)/go swagger generate server --spec=swagger.yaml -t go/src/demoapp \
+            --skip-models \
+            --skip-validation \
+            --client-package=restclient \
+            --name=store \
+            --server-package=storeserver \
+            --operation=healthz \
+            --operation=deleteImageData \
+            --operation=getImageData
+
+GOPATH=$(pwd)/go swagger generate server --spec=swagger.yaml -t go/src/demoapp \
+            --skip-models \
+            --skip-validation \
+            --client-package=restclient \
+            --name=meta \
+            --server-package=metaserver \
+            --operation=healthz \
+            --operation=addImage \
+            --operation=listImages \
+            --operation=getImage \
+            --operation=deleteImage
